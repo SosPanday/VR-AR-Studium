e950b855234e9b67e6ca1c4cc8faabfa5ca6f5d5
Zivono <129958733+Zivono@users.noreply.github.com>
2024-02-06 20:56:40 +0100

    audio vielleicht

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -5,9 +5,26 @@ using UnityEngine.Jobs;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 prevPosition;
+    private int lastWalkSound = 0;
     void Start()
     {
-        transform.GetComponent<AudioSource>().Play();
+        transform.GetComponents<AudioSource>()[0].Play();
+        prevPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if((transform.position - prevPosition).magnitude > 0.7f)
+        {
+            prevPosition = transform.position;
+            PlaySound();
+        }
+    }
+    private void PlaySound()
+    {
+        lastWalkSound += (int)(UnityEngine.Random.value*2) + 1;
+        lastWalkSound %= 3;
+        transform.GetComponents<AudioSource>()[lastWalkSound + 1].Play();
     }
 }
